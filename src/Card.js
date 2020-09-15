8a36867017163b379768a29a6ea37e95fc94bb63
chickenachari <ajitmahto14@gmail.com>
2020-09-16 00:05:59 +0530

    Style

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Card = ({name, position, id}) => {
   return (
-    <div className = 'tc br4 bg-light-red pa3 dib ma2 grow bw2 shadow-3'>
-      <img alt = 'Manchester' src={`https://picsum.photos/id/${id}/200/200?grayscale`} />
+    <div className = 'tc br4 bg-white pa3 dib ma2 grow bw2 shadow-3'>
+      <img alt = 'Manchester' src={`https://picsum.photos/id/${id}/200/200`} />
       <div>
         <h4>{name}</h4>
         <p>{position}</p>
